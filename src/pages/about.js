631dc857773d39f3bcb972ff729c4ae470cf3f65
Monika <m.radosiewicz@osomstudio.com>
2019-04-03 15:26:24 +0200

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,6 +10,7 @@ const About = ({ data: { about } }) => (
       <HelmetDatoCms seo={about.seoMetaTags} />
       <div className="sheet__inner">
         <h1 className="sheet__title">{about.title}</h1>
+        <p>test subtitle before subtitle</p>
         <p className="sheet__lead">{about.subtitle}</p>
         <div className="sheet__gallery">
           <Img fluid={about.photo.fluid} />
